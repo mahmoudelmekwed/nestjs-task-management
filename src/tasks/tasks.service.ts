72db5a3fdcaa76421bbf6720858a56ba15058746
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-08-15 13:27:59 +0300

    implement get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
+import { createTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,11 +13,17 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, descrition: string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find((task)=> task.id === id)
+    }
+
+    createTask(createTaskDto: createTaskDto): Task {
+        const {title, description} = createTaskDto;
+
         const task: Task = {
           id: uuidv4(),
           title,
-          descrition,
+          description,
           status: TaskStatus.OPEN,
         };
     
